f993795b8ec5059e0cb77f06e44ab89b82a9766a
Felipe Ritta <luis.feliperitta@gmail.com>
2022-07-15 17:10:30 -0300

    Exemplos do dia 2 do bloco 7

---
+++ fundamentos/bloco-07-introducao-javascript-es6-testes-unitarios/dia-02-fluxo-de-excecao-e-objetos/exemples/parte2.js
@@ -0,0 +1,57 @@
+const customer1 = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
+
+console.log(customer1);
+
+customer1.lastName = 'Faria';
+console.log(customer1);
+
+const customer2 = {
+  firstName: 'Maria',
+  age: 23,
+  job: 'Medic',
+};
+
+console.log(customer2);
+customer2['lastName'] = 'Silva';
+console.log(customer2);
+
+//
+
+const coolestTvShow = {
+  name: "BoJack Horseman",
+  genre: "adult animation",
+  createdBy: "Raphael Bob-Waksberg",
+  favoriteCharacter: "Princess Carolyn",
+  quote: "Princess Carolyn always lands on her feet.",
+  seasons: 6,
+};
+
+// for (const property in coolestTvShow) {
+//   console.log(property);
+// }
+
+console.log(Object.keys(coolestTvShow));
+
+//
+
+const person = {
+  name: 'Roberto'
+};
+
+const lastName = {
+  lastName: 'Silva'
+};
+
+const clone = Object.assign(person, lastName);
+
+console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
+console.log(person); // { name: 'Roberto', lastName: 'Silva' }
+
+clone.person = 'Ednaldo'
+
+console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
+console.log(person); // { name: 'Roberto', lastName: 'Silva' }
